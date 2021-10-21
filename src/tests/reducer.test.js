32925a96b1dce68b3e8730fdba5cd30c49ea9c88
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-10-21 12:34:30 -0500

    Add tests for reducer

--- src/tests/reducer.test.js
+++ src/tests/reducer.test.js
@@ -1,14 +1,26 @@
-import covidDataReducer from '../redux/Covid-Data-Reducer/covidDataReducer';
-import GetCovidData from '../redux/Covid-Data-Reducer/covidDataReducer';
+// import dependencies
+import React from 'react';
+import { Provider } from 'react-redux';
+import App from '../App';
 
-render(
-  <Provider store={store}>
-    <ChooseIndex />
-  </Provider>
-)
+// import react-testing methods
+import { render } from '@testing-library/react';
+import covidDataReducer from '../redux/Covid-Data-Reducer/covidDataReducer';
+import configureStore from 'redux-mock-store';
 
 describe('Unit tests for reducer', () => {
   const GET_COVID_DATA = 'CovidDataStore/CovidData/GET_COVID_DATA';
+  // const middlewares = [thunk] // add your middlewares like `redux-thunk`
+  const middlewares = []
+  const mockStore = configureStore(middlewares)
+  const initialState = {}
+  const store = mockStore(initialState);
+
+  render (
+    <Provider store={mockStore(initialState)}>
+      <App />
+    </Provider>
+  );
 
   describe('Covid data reducer function test', () => {
     it('returns the correct state for GET_COVID_DATA action', () => {
@@ -25,8 +37,16 @@ describe('Unit tests for reducer', () => {
 
   describe('Reducer display correct Action', () => {
     it('returns GET_COVID_DATA action', () => {
-      expect(GetCovidData().type).toEqual({"Country1": "3", "Country_name": "3", "id": 3});
+      store.dispatch({
+        type: GET_COVID_DATA,
+        state: {
+          id: 3,
+          Country1: '3',
+          Country_name: '3',
+        },
+      })
+        const actions = store.getActions()
+        expect(actions[0].type).toEqual('CovidDataStore/CovidData/GET_COVID_DATA')
     });
   });
-
 });
