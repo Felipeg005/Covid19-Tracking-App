00ab9a4cb84e251a7543392ec07c8595007e4257
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-10-21 10:50:04 -0500

    Add unit test file

--- src/tests/reducer.test.js
+++ src/tests/reducer.test.js
@@ -5,13 +5,13 @@ describe('Unit tests for reducer', () => {
   describe('Reducer display correct Action', () => {
     it('returns the correct state for GET_COVID_DATA action', () => {
       expect(covidDataReducer(state, {
-        type: GET_ROCKETS,
+        type: GET_COVID_DATA,
         state: {
           id: 3,
-          rocket1: '3',
-          rocket_name: '3',
+          Country1: '3',
+          Country_name: '3',
         },
-      })).toEqual([{ id: 1, rocket_name: '1' }, { id: 2, rocket_name: '2' }]);
+      })).toEqual([{ id: 1, Country_name: '1' }, { id: 2, Country_name: '2' }]);
     });
   });
 });
