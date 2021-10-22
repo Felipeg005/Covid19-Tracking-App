0fd952503ea86801d4a074583cf7dac9f3f0189a
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-10-22 12:14:15 -0500

    Add images with color wireframe

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,6 +4,7 @@ import { useHistory } from 'react-router';
 import moment from "moment";
 import mic from '../img/microphone-16.png';
 import settings from '../img/settings-16.png';
+import arrow from '../img/arrow.png';
 
 const date = moment().format("YYYY-MM-DD")
 
@@ -18,11 +19,10 @@ const Nav = () => {
     <nav className='nav-menu'>
       {
         history.location.pathname.includes('details')
-          ? <button type="button" onClick={goBack}>hello</button>
+          ? <button className="go-back-btn" type="button" onClick={goBack}><img className="go-back-img" src={arrow} alt="go"/></button>
           : <h1 className="logo">Covid19 Tracker</h1>
       }
       <ul className='menu-container'>
-        {/* <li className="menu-li">COVID-19 cases worldwide</li> */}
         <li className="menu-date">Date: <br/>{date}</li>
         <button className="mic-button" type="button"><img className="mic-img" src={mic} /><img className="settings-img" src={settings} /></button>
       </ul>
