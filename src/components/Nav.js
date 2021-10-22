69d9969aec9a550588fcc1470c1f61f4ac6dc833
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-10-22 10:25:59 -0500

    Final styles home components

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,11 @@
 /* eslint-disable */
 import React from 'react';
 import { useHistory } from 'react-router';
+import moment from "moment";
+import mic from '../img/microphone-16.png';
+import settings from '../img/settings-16.png';
+
+const date = moment().format("YYYY-MM-DD")
 
 const Nav = () => {
   const history = useHistory();
@@ -17,10 +22,9 @@ const Nav = () => {
           : <h1 className="logo">Covid19 Tracker</h1>
       }
       <ul className='menu-container'>
-        <li className="menu-li">Date</li>
-        <li className="menu-li">COVID-19 cases worldwide</li>
-        <button className="mic-button" type="button">Mic</button>
-        <button className="settings-button" type="button">Settings</button>
+        {/* <li className="menu-li">COVID-19 cases worldwide</li> */}
+        <li className="menu-date">Date: <br/>{date}</li>
+        <button className="mic-button" type="button"><img className="mic-img" src={mic} /><img className="settings-img" src={settings} /></button>
       </ul>
     </nav>
   );
